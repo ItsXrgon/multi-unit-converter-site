8e752b799813b6db53c0027bd7a25324ffaea9d9
Ali Koheil <ali.koheil@gmail.com>
2023-04-07 12:42:46 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/multi-unit-converter-site/',
   plugins: [react()],
+  build: {
+    outDir: 'docs' // Rename the output directory to `docs`
+  }
 })
