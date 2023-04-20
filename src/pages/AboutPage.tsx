31a615dfd6b8b944322a24b38bd23bcb0da402c3
Ali Koheil <ali.koheil@gmail.com>
2023-04-20 07:28:17 +0200

    Added cname

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -56,13 +56,13 @@ function AboutPage() {
           <h2>How to use it:</h2>
           <p>Just select your prefered units from the lists on the left or select a template from the ones available, and the precision (significant figures) you want to round to, then paste the text you wish to convert in the input box and it will automatically convert the units and output them in the output box.</p>
 
-          <h2>How is works 🤓.</h2>
+          <h2>How it works 🤓.</h2>
           <p>The library uses RegEx to detect all possible spellings of units that occur after a number, resolves the aliases of the units into their actual symbol, then converts the numbers using switch statements, rounds to them to the selected precision, the finally replaces them in the actual text. <br /> If you want the code for yourself, report a bug or suggest an improvment, check out the links in the navbar of the website. </p>
           <div className='about-links'>
             <h2>Links:</h2>
             <ul>
-              <li><a href="https://github.com/ItsXrgon">Xrgon</a>: JS library & Website backend</li>
-              <li><a href="https://github.com/ammarmbe">Ammar</a>: Website Frontend.</li>
+              <li><a href="https://github.com/ItsXrgon">Xrgon</a>: JS library & website backend</li>
+              <li><a href="https://github.com/ammarmbe">Ammar</a>: Website frontend.</li>
             </ul>
           </div>
         </div>
